bea5c2c6321af685d9dff001c4f8f031ee49cfa9
TheMaither <themaither@gmail.com>
2024-10-01 09:47:32 +0300

    who is Esac?

--- ftr_query.sh
+++ ftr_query.sh
@@ -5,6 +5,32 @@ while read TOKENS
 do
   for TOKEN in $TOKENS
   do
-    echo $TOKEN
+    # echo $TOKEN
+
+    # if [ $TOKEN = "select" ] 
+    # do
+    #   STATE=select
+    # done
+
+    if [ "$STATE" = select ]
+    then
+      echo "called $STATE with arg $TOKEN"
+      STATE=""
+      continue
+    fi
+
+    case $TOKEN in
+      select )
+        STATE=select
+        ;;
+      
+      put )
+        STATE=put
+        ;;
+
+      * )
+        echo "Unrecognized command $TOKEN" > /proc/self/fd/2
+    esac
+
   done
 done
