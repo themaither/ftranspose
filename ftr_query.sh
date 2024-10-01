f3acd2cf0a1b1e8e39f409b09c20e56da6e0becd
TheMaither <themaither@gmail.com>
2024-10-01 23:28:04 +0300

    Added command executor

--- ftr_query.sh
+++ ftr_query.sh
@@ -1,36 +1,71 @@
 #!/usr/bin/sh
 # Executes given FTranspose query and returns pathes it yielded
 
+# 0 is replased with .
+
+function debug
+{
+  # printf \\e[35m"$@"\\e[0m\\n
+  # printf `echo $@`
+  printf "\\e[2m`echo $@`\\e[0m\\n"
+}
+
+function trace
+{
+  echo $@
+}
+
+NOARG=0
+
+function command_setup_0exit
+{
+  NOARG=1
+}
+
+function command_0exit
+{
+  trace Exiting;
+  exit
+}
+
+function command_setup_0echo
+{
+  NOARG=0
+}
+
+function command_0echo
+{
+  echo "$TOKEN"
+}
+
 while read TOKENS
 do
   for TOKEN in $TOKENS
   do
-    # echo $TOKEN
-
-    # if [ $TOKEN = "select" ] 
-    # do
-    #   STATE=select
-    # done
-
-    if [ "$STATE" = select ]
+    if [ "$COMMAND" = "" ]
     then
-      echo "called $STATE with arg $TOKEN"
-      STATE=""
+      COMMAND=`echo $TOKEN | sed s/\\\./0/`
+
+      debug COMMAND=$COMMAND
+
+      command_setup_$COMMAND
+      debug Set command!
+
+      if [ $NOARG -eq 1 ]
+      then
+      command_$COMMAND
+      debug Executed with no args
+      COMMAND=""
+      ARG=""
+      fi
+
       continue
     fi
-
-    case $TOKEN in
-      select )
-        STATE=select
-        ;;
-      
-      put )
-        STATE=put
-        ;;
-
-      * )
-        echo "Unrecognized command $TOKEN" > /proc/self/fd/2
-    esac
-
+    
+    ARG=$TOKEN
+    command_$COMMAND
+    debug Executed
+    COMMAND=""
+    ARG=""
   done
 done
